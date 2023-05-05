3da64891d9937e6494e3a41e09f6daa0f43fc4eb
Yogi Wisesa <yogiwisesa@gmail.com>
2023-05-05 18:55:00 +0800

    chore: install deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import { type AppType } from "next/dist/shared/lib/utils";
+import {
+  type DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 import "@/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{
+  dehydratedState: DehydratedState;
+}> = ({ Component, pageProps: { dehydratedState, ...pageProps } }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
